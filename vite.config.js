638ad328c7fa4c011fd86a6339f2e64a144f89b3
kaangiray26 <kaangiray26@protonmail.com>
2023-08-20 21:38:27 +0200

    initialized to be used with tauri

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-const path = require('path')
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    root: path.resolve(__dirname, 'src'),
-    server: {
-        port: 8000,
-        hot: true,
-    },
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        }
-    }
-})
+export default defineConfig(async () => ({
+  plugins: [vue()],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+  },
+  // 3. to make use of `TAURI_DEBUG` and other env variables
+  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+  envPrefix: ["VITE_", "TAURI_"],
+}));
